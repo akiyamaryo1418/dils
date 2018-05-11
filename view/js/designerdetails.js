fe8f19ca01979c143b511355adc5425f1bd68dca
WD-1619-12\akiyama-ryo <akiyama-ryo@social-sharing.org>
2018-05-11 10:01:51 +0900

    no message

---
+++ view/js/designerdetails.js
@@ -0,0 +1,77 @@
+$(function(){
+    Initialize();
+});
+
+// 初期化
+function Initialize(){
+	// アドレスの「?」以降のパラメータを取得
+	var adrsid = location.search;
+	// 先頭の「?」をカット
+	adrsid = adrsid.substring(1);
+	id = unescape(adrsid);
+
+	data = {
+		'model'  : 'designer',
+		'action' : 'info',
+		'list'   : id
+	};
+
+	$.ajax({
+		url      : '/dils/api/controller.php',
+		type     : 'POST',
+		dataType : 'json',
+		data     :  data,
+		timeout  :  1000,
+	}).done(function(data, dataType){
+		// ここで値を取得し、表示する
+		//alert('Success');
+	}).fail(function(){
+		alert('NoData');
+	});
+}
+
+// 前のページへ移動(戻るボタン)
+function moveBackButton(){
+	location.href = "/dils/view/html/designerindex.html";
+}
+
+// ソート時のボタン(非同期)
+function sortButton(){
+	var param = "";
+
+	data = {
+		'model'  : 'indexsort',
+		'action' : 'sort',
+		'list'   :  param
+	};
+
+	$.ajax({
+		url      : '/dils/api/controller.php',
+		type     : 'POST',
+		dataType : 'json',
+		data     :  data,
+		timeout  :  1000,
+	}).done(function(data, dataType){
+		alert('Success');
+	}).fail(function(){
+		alert('NoData');
+	});
+}
+
+//フィルタ検索機能(ジャンル)(非同期)
+function searchCategory(){
+
+	var category = $('#category_id').val();
+
+	$.ajax({
+		url      : '/dils/api/controller.php',
+		type     : 'POST',
+		dataType : 'json',
+		data     :  data,
+		timeout  :  1000,
+	}).done(function(data, dataType){
+		alert('Success');
+	}).fail(function(){
+		alert('NoData');
+	});
+}
