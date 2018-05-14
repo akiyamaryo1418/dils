b061f8e64c00dd3df63e0fc8a392d3a953074823
WD-1722-01\inoue-saki <inoue-saki@social-sharing.org>
2018-05-14 13:15:34 +0900

    masonryのプラグイン導入

--- view/js/style.js
+++ view/js/style.js
@@ -7,7 +7,11 @@ $('.slider-wrapper').slick({
   // 自動再生で切り替えする時間(ミリ秒) [初期値:3000]
 });
 
-
+$('#container').masonry({
+    itemSelector: '.item',
+    columnWidth: 180, 
+    isFitWidth: true  //親要素の幅に合わせてカラム数を自動調整
+  });
 
 
 });
