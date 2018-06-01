4efbe7377f465b5c5529a5513513d9f499484e37
WD-1722-01\inoue-saki <inoue-saki@social-sharing.org>
2018-06-01 15:27:25 +0900

    no message

---
+++ view/js/indexlightbox.js
@@ -0,0 +1,88 @@
+// ライトボックスを開く
+function openLightbox(id,pass){
+
+	var data = {
+		'model'  : 'evaluation',
+		'action' : 'index',
+		'list'   :  id
+	}
+
+	$.ajax({
+        url      : '../../api/controller.php',
+        type     : 'POST',
+        dataType : 'json',
+        data     :  data,
+        timeout  :  1000,
+	}).done(function(data, dataType){
+		$('.imgbox').append('<img class="lightboxview" id="lightboxid_"'+id+'"  src="'+pass+'">')
+		$(".lightbox_view, #lightboxid_"+id+"").fadeIn();   // 第1引数・・・背景？(class)  第2引数・・・拡大写真(class)
+		$('body').addClass("overflow");
+		lightboxtriming();
+
+		var intaverage = Math.floor(data[0].review);
+
+		for(var index = 0; index < data.length; index++){
+			$('.commentbox').append($('<dl class="lightboxview"></dl>')
+                            .append($('<dt></dt>').html(data[index].created_at))
+                            .append($('<dd></dd>').html(data[index].comment)));
+		}
+
+		var intaverage =  6 - Math.floor(data[0].review);
+		if(intaverage != 6){
+			$('#star'+intaverage+'').attr({'checked': 'checked'});
+		}
+
+		// 見えないようにしている
+		$('.idmem').append($('<input type="radio" name="illustid" value="'+id+'" class="id" checked="checked" display:none>'));
+	}).fail(function(){
+        alert('no');
+	})
+}
+
+function lightboxtriming(){
+	var resizeClass    = '.imgbox img';
+	var thumnailHeight = 700;
+	var thumnailWidth  = 750;
+	var iw, ih;
+
+	$(resizeClass).each(function(){
+		var w = $(this).width();   // 画像の幅(原寸)
+		var h = $(this).height();  // 画像の高さ(原寸)
+
+		// 横長の画像の場合
+		if(w >= h){
+			iw = (thumnailHeight / h * w - thumnailWidth) / 2
+			$(this).height(thumnailHeight);    // 高さをサムネイルに合わせる
+			$(this).css("top", 0);
+			$(this).css("left", "-"+iw+"px");  // 画像のセンター合わせ
+		}
+
+		// 縦長の画像の場合
+		else{
+			ih = (thumnailWidth / w * h - thumnailHeight) / 2
+			$(this).width(thumnailWidth);      // 幅をサムネイルに合わせる
+			$(this).css("top","-"+ih+"px");    // 画像のセンター合わせ
+			$(this).css("left", 0);
+		}
+
+		//====固定値====
+		$(this).height(thumnailHeight);
+		$(this).width(thumnailWidth);
+		$(this).css("height", 700+"px");
+		$(this).css("top", 0);
+		$(this).css("width", 750+"px");
+		$(this).css("left", 0);
+        //==============
+	});
+}
+
+//ライトボックスを閉じる
+function closeLightbox(){
+	var memid = $('.id').val();
+
+	//alert(memid);
+	$(".lightbox_view, #lightboxid_"+memid+"").fadeOut();
+	$('.lightboxview').remove();
+	$('.id').remove();
+	$('body').removeClass("overflow");
+}
--- view/js/style.js
+++ view/js/style.js
@@ -5,6 +5,7 @@ $('.slider-wrapper').slick({
   // 自動再生するか [初期値:false]
   autoplay: true
   // 自動再生で切り替えする時間(ミリ秒) [初期値:3000]
+
 });
 
 $('#container').masonry({
